873fe64b7cfe604017a508b0b4ab7f216e2181ea
Samer-Ismael <samerismaeelbat2@hotmail.com>
2024-02-14 10:52:09 +0100

    Saving

--- src/main/java/com/frontend/CatFrontend/controller/CatController.java
+++ src/main/java/com/frontend/CatFrontend/controller/CatController.java
@@ -20,37 +20,61 @@ public class CatController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<Cat> addCat(@RequestBody Cat cat){
-        return ResponseEntity.ok(catRepo.save(cat));
+    public ResponseEntity<Cat> addCat(@RequestBody Cat cat) {
+        try {
+            return ResponseEntity.ok(catRepo.save(cat));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Cat>> getAllCats(){
-        return ResponseEntity.ok(catRepo.findAll());
+    public ResponseEntity<List<Cat>> getAllCats() {
+
+        try {
+            return ResponseEntity.ok(catRepo.findAll());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
     }
 
     @GetMapping("/{name}")
-    public ResponseEntity<Cat> getCatByName(@PathVariable String name){
+    public ResponseEntity<Cat> getCatByName(@PathVariable String name) {
 
-        if (catRepo.findByName(name).isPresent()){
-            return ResponseEntity.ok(catRepo.findByName(name).get());
-        }else {
-            return ResponseEntity.notFound().build();
+        try {
+            if (catRepo.findByName(name).isPresent()) {
+                return ResponseEntity.ok(catRepo.findByName(name).get());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
         }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Cat> updateCat(@PathVariable int id, @RequestBody Cat cat){
-        Cat cat1 = catRepo.findById(id).get();
-        cat1.setName(cat.getName());
-        cat1.setColor(cat.getColor());
-        cat1.setAge(cat.getAge());
-        return ResponseEntity.ok(catRepo.save(cat1));
+    public ResponseEntity<Cat> updateCat(@PathVariable int id, @RequestBody Cat cat) {
+
+        try {
+            Cat cat1 = catRepo.findById(id).get();
+            cat1.setName(cat.getName());
+            cat1.setColor(cat.getColor());
+            cat1.setAge(cat.getAge());
+            return ResponseEntity.ok(catRepo.save(cat1));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteCat(@PathVariable int id){
-        catRepo.deleteById(id);
-        return ResponseEntity.ok("Cat Deleted");
+    public ResponseEntity<String> deleteCat(@PathVariable int id) {
+        try {
+            catRepo.deleteById(id);
+            return ResponseEntity.ok("Cat Deleted");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
